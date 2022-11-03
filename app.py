b25199f1c2c0ea3ca5804730cc69350a2130b672
Gihan Rangana <gihanrangana.mr@gmail.com>
2022-11-04 01:34:57 +0530

    Update app.py
    Update Print message

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# import socket
-# from datetime import datetime
-
-# date_time = datetime.now()
-# timestamp = datetime.timestamp(date_time)
-
-# print("Date and time is ", date_time)
-# print("Timestamp is ", timestamp)
-# print("Hostname is " + socket.gethostname())
-
 from flask import Flask
 import os
 import socket
@@ -20,7 +10,7 @@ def hello_world():
     date_time = datetime.now()
     timestamp = datetime.timestamp(date_time)
     hostname  = socket.gethostname()
-    return ("Date: " + str(date_time ) + "<br/>Timestamp: " +str(timestamp) + "<br/>Hostname: " + str(hostname) )
+    return ("Date and Time : " + str(date_time ) + "<br/>Timestamp: " +str(timestamp) + "<br/>Hostname: " + str(hostname) )
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 4000))
